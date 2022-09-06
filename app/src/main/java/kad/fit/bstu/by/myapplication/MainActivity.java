9ca0c22df857787d92df0743d7ba315fba8be518
Arseni <Kravzhul2002@mail.ru>
2022-09-07 00:50:24 +0300

    Initial commit

---
+++ app/src/main/java/kad/fit/bstu/by/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package kad.fit.bstu.by.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO 222
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/kad/fit/bstu/by/myapplication/text/TextFunction.java
@@ -0,0 +1,8 @@
+package kad.fit.bstu.by.myapplication;
+
+public class TextFunction {
+    public String getValue(){
+        return "Hello from first project";
+    }
+
+}
